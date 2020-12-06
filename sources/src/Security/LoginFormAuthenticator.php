4dc63c7a1bacfb44c39b26afcbb5b710284970d4
TOTOleHero (Thomas Lecarpentier) <thomas.lecarpentier@gmail.com>
2020-12-06 19:58:06 +0100

    add login and register page

--- sources/src/Security/LoginFormAuthenticator.php
+++ sources/src/Security/LoginFormAuthenticator.php
@@ -97,6 +97,8 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
+        return new RedirectResponse($this->urlGenerator->generate('home'));
+        
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
         // redirect to some "app_homepage" route - of wherever you want
         // return new RedirectResponse($this->urlGenerator->generate('app_homepage'));
